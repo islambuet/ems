d17ac209c7cf1fd4a3296bfea16e8913addfd5c2
islambuet <islambuet@gmail.com>
2016-03-18 01:25:54 +0600

    current stock calculation fixed at sales model

--- application/models/Sales_model.php
+++ application/models/Sales_model.php
@@ -49,9 +49,7 @@ class Sales_model extends CI_Model
         {
             $stocks[$result['variety_id']][$result['pack_size_id']]['stock_in']=$result['stock_in'];
             $stocks[$result['variety_id']][$result['pack_size_id']]['excess']=0;
-            $stocks[$result['variety_id']][$result['pack_size_id']]['sales_return']=0;
             $stocks[$result['variety_id']][$result['pack_size_id']]['stockout']=0;
-            $stocks[$result['variety_id']][$result['pack_size_id']]['sample']=0;
             $stocks[$result['variety_id']][$result['pack_size_id']]['sales']=0;
 
             $stocks[$result['variety_id']][$result['pack_size_id']]['current_stock']=$result['stock_in'];
@@ -72,10 +70,7 @@ class Sales_model extends CI_Model
             $stocks[$result['variety_id']][$result['pack_size_id']]['excess']=$result['stock_in'];
             $stocks[$result['variety_id']][$result['pack_size_id']]['current_stock']+=$result['stock_in'];
         }
-        //+sales return pending
-        //sales bonus return pending
-
-        //-stock out
+        //-stock out all
         $this->db->from($CI->config->item('table_stockout'));
         $this->db->select('variety_id,pack_size_id');
         $this->db->select('SUM(quantity) stockout');
@@ -91,11 +86,11 @@ class Sales_model extends CI_Model
             $stocks[$result['variety_id']][$result['pack_size_id']]['stockout']=$result['stockout'];
             $stocks[$result['variety_id']][$result['pack_size_id']]['current_stock']-=$result['stockout'];
         }
-        //-sales
+        //-sales and sales return
 
         $this->db->from($CI->config->item('table_sales_po_details').' spd');
         $this->db->select('variety_id,pack_size_id');
-        $this->db->select('SUM(quantity) sales');
+        $this->db->select('SUM(quantity-quantity_return) sales');
         $this->db->join($CI->config->item('table_sales_po').' sp','sp.id =spd.sales_po_id','INNER');
         $this->db->group_by(array('variety_id','pack_size_id'));
         if(strlen($where)>0)
@@ -105,19 +100,17 @@ class Sales_model extends CI_Model
         $this->db->where('sp.status_approved',$CI->config->item('system_status_po_approval_approved'));
         $this->db->where('spd.revision',1);
         $results=$this->db->get()->result_array();
-        /*echo '<PRE>';
-        print_r($results);
-        echo '</PRE>';*/
+
         foreach($results as $result)
         {
             $stocks[$result['variety_id']][$result['pack_size_id']]['sales']=$result['sales'];
             $stocks[$result['variety_id']][$result['pack_size_id']]['current_stock']-=$result['sales'];
         }
 
-        //-sales bonus
+        //-sales bonus and bonus return
         $this->db->from($CI->config->item('table_sales_po_details').' spd');
         $this->db->select('variety_id,bonus_pack_size_id pack_size_id');
-        $this->db->select('SUM(quantity_bonus) sales');
+        $this->db->select('SUM(quantity_bonus-quantity_bonus_return) sales');
         $this->db->join($CI->config->item('table_sales_po').' sp','sp.id =spd.sales_po_id','INNER');
         $this->db->group_by(array('variety_id','bonus_pack_size_id'));
         if(strlen($where_bonus)>0)
@@ -128,9 +121,7 @@ class Sales_model extends CI_Model
         $this->db->where('sp.status_approved',$CI->config->item('system_status_po_approval_approved'));
         $this->db->where('spd.revision',1);
         $results=$this->db->get()->result_array();
-        /*echo '<PRE>';
-        print_r($results);
-        echo '</PRE>';*/
+
         foreach($results as $result)
         {
             $stocks[$result['variety_id']][$result['pack_size_id']]['sales']+=$result['sales'];
@@ -158,7 +149,7 @@ class Sales_model extends CI_Model
         }
         //purchase
         $this->db->from($CI->config->item('table_sales_po_details').' spd');
-        $this->db->select('SUM(spd.variety_price*spd.quantity) total_buy');
+        $this->db->select('SUM(spd.variety_price*(spd.quantity-spd.quantity_return)) total_buy');
         $this->db->join($CI->config->item('table_sales_po').' sp','sp.id = spd.sales_po_id','INNER');
         $this->db->where('sp.customer_id',$customer_id);
         $this->db->where('spd.revision',1);
@@ -168,7 +159,7 @@ class Sales_model extends CI_Model
         {
             $current_credit+=$result['total_buy'];//plus for relative to arm
         }
-        //sales _return and return bonus pending
+
         return $current_credit;
 
     }
