3064d55b8bc3d43f5e9c09b634a245bdde2b8c6c
islambuet <islambuet@gmail.com>
2016-03-17 22:10:49 +0600

    Po receive task included area

--- application/controllers/Sales_po_receive.php
+++ application/controllers/Sales_po_receive.php
@@ -11,6 +11,23 @@ class Sales_po_receive extends Root_Controller
         parent::__construct();
         $this->message="";
         $this->permissions=User_helper::get_permission('Sales_po_receive');
+        $this->locations=User_helper::get_locations();
+        if(!is_array($this->locations))
+        {
+            if($this->locations=='wrong')
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line('MSG_LOCATION_INVALID');
+                $this->jsonReturn($ajax);
+            }
+            else
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line('MSG_LOCATION_NOT_ASSIGNED');
+                $this->jsonReturn($ajax);
+            }
+
+        }
         $this->controller_url='sales_po_receive';
         $this->load->model("sales_model");
     }
@@ -96,6 +113,13 @@ class Sales_po_receive extends Root_Controller
             if(!$data['po'])
             {
                 System_helper::invalid_try($this->config->item('system_edit_not_exists'),$po_id);
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+                $this->jsonReturn($ajax);
+            }
+            if(!$this->check_my_editable($data['po']))
+            {
+                System_helper::invalid_try($this->config->item('system_edit_others'),$po_id);
                 $ajax['status']=false;
                 $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
                 $this->jsonReturn($ajax);
@@ -200,6 +224,13 @@ class Sales_po_receive extends Root_Controller
             if(!$data['po'])
             {
                 System_helper::invalid_try($this->config->item('system_view_not_exists'),$po_id);
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+                $this->jsonReturn($ajax);
+            }
+            if(!$this->check_my_editable($data['po']))
+            {
+                System_helper::invalid_try($this->config->item('system_view_others'),$po_id);
                 $ajax['status']=false;
                 $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
                 $this->jsonReturn($ajax);
@@ -424,6 +455,26 @@ class Sales_po_receive extends Root_Controller
         }
         return true;
     }
+    private function check_my_editable($customer)
+    {
+        if(($this->locations['division_id']>0)&&($this->locations['division_id']!=$customer['division_id']))
+        {
+            return false;
+        }
+        if(($this->locations['zone_id']>0)&&($this->locations['zone_id']!=$customer['zone_id']))
+        {
+            return false;
+        }
+        if(($this->locations['territory_id']>0)&&($this->locations['territory_id']!=$customer['territory_id']))
+        {
+            return false;
+        }
+        if(($this->locations['district_id']>0)&&($this->locations['district_id']!=$customer['district_id']))
+        {
+            return false;
+        }
+        return true;
+    }
     public function get_items()
     {
         $this->db->from($this->config->item('table_sales_po_details').' pod');
@@ -446,7 +497,22 @@ class Sales_po_receive extends Root_Controller
         $this->db->join($this->config->item('table_setup_location_territories').' t','t.id = d.territory_id','INNER');
         $this->db->join($this->config->item('table_setup_location_zones').' zone','zone.id = t.zone_id','INNER');
         $this->db->join($this->config->item('table_setup_location_divisions').' division','division.id = zone.division_id','INNER');
-
+        if($this->locations['division_id']>0)
+        {
+            $this->db->where('division.id',$this->locations['division_id']);
+            if($this->locations['zone_id']>0)
+            {
+                $this->db->where('zone.id',$this->locations['zone_id']);
+                if($this->locations['territory_id']>0)
+                {
+                    $this->db->where('t.id',$this->locations['territory_id']);
+                    if($this->locations['district_id']>0)
+                    {
+                        $this->db->where('d.id',$this->locations['district_id']);
+                    }
+                }
+            }
+        }
         $this->db->where('pod.revision',1);
         $this->db->where('po.status_delivered',$this->config->item('system_status_po_delivery_delivered'));
         $this->db->group_by('po.id');
