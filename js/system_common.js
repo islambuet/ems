1785a62fffd7f7fb43a3f8e206e80f5a32c511a5
islambuet <islambuet@gmail.com>
2016-05-29 10:25:16 +0600

    Download as csv license use changed just plain html

--- js/system_common.js
+++ js/system_common.js
@@ -348,8 +348,24 @@ $(document).ready(function()
     });
     $(document).on("click", "#button_action_csv", function(event)
     {
+        //previous csv file
+        /*var jqxgrid_id='#system_jqx_container';
+        $(jqxgrid_id).jqxGrid('exportdata', 'csv', $(this).attr('data-title'));*/
         var jqxgrid_id='#system_jqx_container';
-        $(jqxgrid_id).jqxGrid('exportdata', 'csv', $(this).attr('data-title'));
+
+        var gridContent = $(jqxgrid_id).jqxGrid('exportdata', 'html');
+        var newWindow = window.open('', '', 'width=800, height=500,menubar=yes,toolbar=no,scrollbars=yes'),
+            document = newWindow.document.open(),
+            pageContent =
+                '<!DOCTYPE html>\n' +
+                    '<html>\n' +
+                    '<head>\n' +
+                    '<meta charset="utf-8" />\n' +
+                    '<title>'+$(this).attr('data-title')+'</title>\n' +
+                    '</head>\n' +
+                    '<body>\n' + gridContent + '\n</body>\n</html>';
+        document.write(pageContent);
+        document.close();
 
     });
     $(document).on("click", ".system_jqx_column", function(event)
