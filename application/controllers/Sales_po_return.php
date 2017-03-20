ac37c670b16f3f8b3f9ad3a5ec11dc1c6afbc673
islambuet <islambuet@gmail.com>
2017-03-21 02:07:03 +0600

    sales return load more added

--- application/controllers/Sales_po_return.php
+++ application/controllers/Sales_po_return.php
@@ -438,7 +438,20 @@ class Sales_po_return extends Root_Controller
     }
     public function get_items()
     {
-
+        $current_records = $this->input->post('total_records');
+        if(!$current_records)
+        {
+            $current_records=0;
+        }
+        $pagesize = $this->input->post('pagesize');
+        if(!$pagesize)
+        {
+            $pagesize=40;
+        }
+        else
+        {
+            $pagesize=$pagesize*2;
+        }
         $this->db->from($this->config->item('table_sales_po_details').' pod');
 
         $this->db->select('SUM(pod.quantity) quantity_total');
@@ -485,6 +498,7 @@ class Sales_po_return extends Root_Controller
         $this->db->where('po.status_received',$this->config->item('system_status_po_received_received'));
         $this->db->group_by('po.id');
         $this->db->order_by('po.id','DESC');
+        $this->db->limit($pagesize,$current_records);
         $items=$this->db->get()->result_array();
         foreach($items as &$item)
         {
